8be4c0a61254b0c7b63c98db184b7b7f5102d625
EmotionsDesigner <claymore200@gmail.com>
2016-03-02 22:39:40 +0100

    First Backup

---
+++ Assets/Scripts/Enemy/MoveBetweenObjects.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBetweenObjects : MonoBehaviour {
+
+    //referencja do ZombieAI
+    public ZombieAI ZombieAI;
+
+      //cele statyczne (między którymi porusza się zombie, dopóki gracz nie będzie w jego zasięgu)
+    public GameObject[] staticTargets;
+    //w jakiej odległości od celu wybrać już kolejny cel
+    public float pickNextTarget;
+    //cel statyczny, do którego aktualnie zmierza (null jeżeli zmierza do gracza
+    GameObject actualStaticTarget;
+    //id w tablicy aktualnego celu
+    int actualTargetID;
+    GameObject closestTarget;
+    //określa czy poruszamy się do kolejnych czy poprzednich el. tablicy
+    bool movingForward;
+    //liczba statycznych celów
+    int targetsAmount;
+
+
+      //ustawia aktualny cel statyczny na najbliższy potworowi
+    void SetNewStaticTarget()
+    {
+        if (staticTargets.Length>0){
+            closestTarget = staticTargets[0];
+            float closestDistance = Vector3.Distance(transform.position, staticTargets[0].transform.position);
+            int id = 0;
+            foreach (GameObject staticTarget in staticTargets)
+            {
+                //odległość od celu statycznego
+                float distance = Vector3.Distance(transform.position, staticTarget.transform.position);
+           
+                if (distance < closestDistance)
+                {
+                    closestDistance=distance;
+                    closestTarget = staticTarget;
+                    actualTargetID = id;
+                    actualStaticTarget = closestTarget;
+                }
+                id++;
+            }
+         }
+    }
+
+    //potwór porusza się między kolejnymi celami z tablicy, dopóki gracz nie znajdzie się w jego zasięgu
+    void MoveBetweenStaticTargets() {
+  
+          //ustalenie aktualnego celu
+            ZombieAI.target = actualStaticTarget;
+              //odległość od aktualnego celu statycznego
+            float targetDistance = Vector3.Distance(ZombieAI.transform.position, actualStaticTarget.transform.position);
+            //jeżeli jesteśmy dostatecznie blisko
+            if (targetDistance < pickNextTarget)
+            {
+                //idziemy "do przodu"
+                if (movingForward) { 
+                        if (actualTargetID < targetsAmount - 1){
+                            //ustawienie celu
+                            actualTargetID++;
+                            actualStaticTarget = staticTargets[actualTargetID];
+                        }
+                        else{
+                            //ustawiamy flagę, od teraz idziemy "do tyłu"
+                            movingForward = false;
+                            //ustawienie celu
+                            actualTargetID--;
+                            actualStaticTarget = staticTargets[actualTargetID];
+                        }
+                }
+                //idziemy "do tyłu
+                else{
+                    if (actualTargetID > 0){
+                        //ustawienie celu
+                        actualTargetID--;
+                        actualStaticTarget = staticTargets[actualTargetID];
+                    }
+                    else {
+                        //ustawiamy flagę, od teraz "do przodu"
+                         movingForward = true;
+                         //ustawienie celu
+                         actualTargetID++;
+                         actualStaticTarget = staticTargets[actualTargetID];
+                    }
+                }
+            }
+        }
+    
+
+
+
+	// Use this for initialization
+	void Start () {
+	if (staticTargets.Length < 2)
+            Debug.LogError("min 2 cele statyczne");
+    //poruszanie między celami w kolejności takiej jakiej są zapisane w tablicy, gdy dojdzie do końca, cofa się do początku itd.
+    actualTargetID = 0;
+    actualStaticTarget = staticTargets[actualTargetID];
+    movingForward = true;
+    targetsAmount = staticTargets.Length;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        //gracz jest poza zasięgiem
+        if (!ZombieAI.isHunting)
+            MoveBetweenStaticTargets();
+            //gdy atakujemy gracza, ustawiamy nowy cel statyczny do którego wrócimy, gdy gracz ucieknie
+        else
+            SetNewStaticTarget();
+
+
+
+	}
+}
---
+++ Assets/Scripts/Enemy/ZombieAI.cs
@@ -0,0 +1,184 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pathfinding;
+
+public class ZombieAI : MonoBehaviour {
+    public CharacterController controller;
+    public GameObject Player;
+    //referencja do wyszukiwacza ścieżki 
+    public AIPath AIPath;
+
+    //cel AKUTALNY
+    public GameObject target;
+
+    public float playerDistance;
+    Vector3 TargetPosition;
+
+  
+
+    //właściwości obiektu
+    public float attackDistance;
+    //poniżej tej odległości zombie zaczyna podążać za graczem
+    public float huntingRange;
+    public float health;
+    public float speed;
+    float approximation=1f;
+    Animator animator;
+    bool attackPlaying = false;
+    //w momencie śmierci przyjmuje wartość true
+    bool deathMoment=false;
+    bool attack = false;
+
+    //czy aktualnie atakuje/poluje na gracza
+    public bool isHunting;
+  
+
+
+
+
+    void OnCollisionEnter(Collision col)
+    {
+        //Debug.Log(col.transform.name);
+    }
+
+    void StartHunting()
+    {
+        isHunting = true;
+        target = Player;
+    }
+    void StopMoving()
+    {
+        isHunting= false;
+         //przerwanie pościgu i wyszukiwania ścieżki
+            AIPath.canMove = false;
+            AIPath.canSearch = false;
+    }
+
+
+    struct direction
+    {
+        public bool x;
+        public bool y;
+        public bool z;
+      public void setX(){
+          x=true;
+          y=false;
+          z=false;
+      }
+        public void setY(){
+          x=false;
+          y=true;
+          z=false;
+      }
+        public void setZ(){
+          x=false;
+          y=false;
+          z=true;
+      }
+    };
+
+    //zwraca wartość, do którego dana współrzędna powinna dążyć
+    float ChooseDirection(direction dir)
+    {
+        if (dir.x)
+        {
+            if (transform.position.x+approximation < TargetPosition.x)
+                return speed;
+            else if (transform.position.x-approximation > TargetPosition.x)
+                return -speed;
+            else
+                return 0;
+        }
+       if (dir.y)
+        {
+            if (transform.position.y+approximation < TargetPosition.y)
+                return speed;
+            else if (transform.position.y-approximation > TargetPosition.y)
+                return -speed;
+            else return 0;
+        }
+        if (dir.z)
+        {
+            if (transform.position.z +approximation< TargetPosition.z)
+                return speed;
+            else if (transform.position.z-approximation > TargetPosition.z)
+                return -speed;
+            else return 0;
+        }
+       return 0f;//zła wartość (tylko x,y)
+    }
+
+    IEnumerator AudioPlay(string name, float delay)
+    {
+
+        attackPlaying = true;
+        AudioSource audio = GameObject.Find(name).GetComponent<AudioSource>();
+        audio.Play();
+        yield return new WaitForSeconds(delay);
+        attackPlaying = false;
+    }
+
+
+
+	// Use this for initialization
+	void Start () { 
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent < Animator>();
+  
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        //uaktualnienie punktów w życia w kontrolerze animacji
+        animator.SetFloat("Health", health);
+         animator.SetBool("Attack", attack);
+        //uaktualnienie odległości od gracza
+        TargetPosition = target.transform.position;
+        playerDistance = Vector3.Distance(transform.position, Player.transform.position);
+
+        //moment śmierci
+        if (health <= 0 && deathMoment==false)
+        {
+            deathMoment = true;
+            StopMoving();
+
+            //dezaktywuje siatki kolizji dla wszystkich części ciała umarłego zombie
+            MeshCollider[] meshColliderChildren = gameObject.GetComponentsInChildren<MeshCollider>();
+            foreach (MeshCollider meshChildCollider in meshColliderChildren)
+                meshChildCollider.enabled = false;      
+            AudioPlay("ZombieDead", 0);
+        }
+
+        if (health > 0)
+        {   
+               
+                //pościg/atak gracza
+            if (playerDistance < huntingRange)
+            {
+                StartHunting();
+                //atak
+                if (playerDistance <= attackDistance)
+                {
+                    isHunting = false;
+                    //odwrócenie w strone gracza
+                    transform.LookAt(target.transform);
+                    attack = true;
+                    if (attackPlaying == false)
+                        StartCoroutine(AudioPlay("ZombieAttack", 2f));
+                }
+                else
+                {
+                    StartHunting();
+                    attack = false;
+                }
+
+            }
+            else
+                isHunting = false;
+      }
+         //zombie nie żyje
+            else {
+                 isHunting = false;
+            }
+	}
+}
