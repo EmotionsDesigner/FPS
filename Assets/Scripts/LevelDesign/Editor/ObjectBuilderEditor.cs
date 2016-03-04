31f43ac23995694e5f692c64c1f1d3d4edbb4a42
EmotionsDesigner <claymore200@gmail.com>
2016-03-04 14:11:47 +0100

    Utworzono mapę i źródło dźwięku.
    Gracz ma podążać za głosami. Ustawić zomiaków i lampy.

---
+++ Assets/Scripts/LevelDesign/Editor/ObjectBuilderEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(WallsDesigner))]
+public class ObjectBuilderEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        
+        WallsDesigner myScript = (WallsDesigner)target;
+        if(GUILayout.Button("Build Corridor"))
+        {
+            myScript.BuildCorridor();
+        }
+    }
+}
